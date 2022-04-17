7e60b4fb98e62d414b2d06f42e0d0e061d252e81
chon9496 <87382634+chon9496@users.noreply.github.com>
2022-04-17 12:56:39 -0500

    BlogDetailView

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render,redirect
+from django.shortcuts import render,redirect, get_object_or_404
 from django.views.generic import View
 from .forms import PostCreateForm
 from .models import Post
 
 class BlogListView(View):
     def get(self,request,*args, **kwargs):
+        posts = Post.objects.all()
         context={
-
+            'posts':posts
         }
         return render(request,'blog_list.html',context)
 
@@ -20,7 +21,7 @@ class BlogCreateView(View):
     
     def post(self,request,*args, **kwargs):
         if request.method=='POST':
-            form= PostCreateForm(request.POST)
+            form = PostCreateForm(request.POST)
             if form.is_valid():
                 title   = form.cleaned_data.get('title')
                 content = form.cleaned_data.get('content')
@@ -32,3 +33,11 @@ class BlogCreateView(View):
 
         }
         return render(request,'blog_create.html',context)
+
+class BlogDetailView(View):
+    def get (self, request,pk,*args, **kwargs):
+        post = get_object_or_404(Post,pk=pk)
+        context={
+             'post':post    
+        }
+        return render(request,'blog_detail.html',context)
