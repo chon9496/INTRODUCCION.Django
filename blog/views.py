c2579c593c240d63faaabe84de457122d6f70b53
chon9496 <87382634+chon9496@users.noreply.github.com>
2022-04-17 00:51:16 -0500

    BlogCreateView

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.views.generic import View
+from .forms import PostCreateForm
 
 class BlogListView(View):
     def get(self,request,*args, **kwargs):
@@ -9,3 +8,16 @@ class BlogListView(View):
 
         }
         return render(request,'blog_list.html',context)
+
+class BlogCreateView(View):
+    def get(self,request,*args, **kwargs):
+        context={
+
+        }
+        return render(request,'blog_create.html',context)
+    
+    def post(self,request,*args, **kwargs):
+        context={
+
+        }
+        return render(request,'blog_create.html',context)
